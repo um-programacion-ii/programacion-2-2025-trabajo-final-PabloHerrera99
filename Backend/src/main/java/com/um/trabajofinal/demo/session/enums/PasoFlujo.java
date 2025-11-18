7780b361ba4a7c0e725767fd35c3906f46c7fd85
PabloHerrera99 <p.herrera@alumno.um.edu.ar>
2025-11-18 00:19:39 -0300

    Se creo la entidad sesion usuario

---
+++ Backend/src/main/java/com/um/trabajofinal/demo/session/enums/PasoFlujo.java
@@ -0,0 +1,10 @@
+package com.um.trabajofinal.demo.session.enums;
+
+public enum PasoFlujo {
+    PASO_1_LISTA_EVENTOS,
+    PASO_2_EVENTO_SELECCIONADO,
+    PASO_3_ASIENTOS_SELECCIONADOS,
+    PASO_4_DATOS_PERSONAS,
+    PASO_5_VENTA_CONFIRMADA
+}
+
